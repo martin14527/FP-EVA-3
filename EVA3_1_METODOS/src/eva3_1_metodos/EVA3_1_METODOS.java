3ee762485ed0169a93b345525657677350dee46f
martin14527 <guillermovallejoloya@gmail.com>
2025-11-05 22:34:39 -0600

    Add files via upload

---
+++ EVA3_1_METODOS/src/eva3_1_metodos/EVA3_1_METODOS.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_1_metodos;
+public class EVA3_1_METODOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        int val1 = 5, val2=10;
+        int resu = SumarEnteros(val1,val2);
+         System.out.println(resu);
+    }
+    //sumar dos enteros
+    //lamar metodos como verbos
+    //modificaciones de texto public, protected, private.
+    // static es necesario en el maint (dentro de)
+    //valor de retorno, cualquier tipo de valor
+    //mismas reglas que en identificadores, en los nombres
+    //lista de parametros:pueden ser 0 o N cantidad
+    public static void SumarEnteros(int num1,int num2){
+    //solo necesito ejecutar el metodo
+        int suma = num1+ num2;
+}
+    
+    //identificador del metodo    
+    
+}
